993d17fe0b4365ba101c374acba8801e44af3d69
alswn8477 <alswn8477@naver.com>
2025-05-27 11:14:18 +0900

    first commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// script.js
-function loadAlert() {
-  alert("Test!");
-}
+$("#fullpage").fullpage({
+  autoScrolling: true,
+  navigation: true,
+});
 
 // header
 // let header = document.querySelector("header");
@@ -23,66 +23,3 @@ function loadAlert() {
 //   logo.querySelector("img:nth-child(2)").style.display = "none";
 //   document.querySelector(".con").style.filter = "none";
 // };
-
-// const con1 = document.querySelector(".con");
-// const height = con1.offsetHeight;
-// console.log(height);
-
-// window.addEventListener("scroll", function () {
-//   const sideBtna = this.document.querySelector(".sideBtn a");
-//   const sideBtn = this.document.querySelector(".sideBtn");
-//   const triggerHeight = 2000;
-//   if (this.window.scrollY > 200) {
-//     sideBtna.style.opacity = "1";
-//   } else {
-//     sideBtna.style.opacity = "0";
-//   }
-//   if (this.window.pageYOffset > triggerHeight) {
-//     sideBtn.classList.add("on");
-//   } else {
-//     sideBtn.classList.remove("on");
-//   }
-// });
-
-//con3
-// let con3ul = document.querySelector(".con3 .wrap ul");
-// let divs = document.querySelectorAll(".con3 .wrap .photo > div");
-
-// con3ul.querySelectorAll("li").forEach(function (li, index) {
-//   li.addEventListener("mouseenter", function () {
-//     divs.forEach(function (div) {
-//       div.style.display = "none";
-//     });
-
-//     divs[index].style.display = "block";
-//   });
-// });
-
-// con6
-// new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   spaceBetween: 100,
-//   grabCursor: true,
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//     draggable: true,
-//   },
-// });
-
-// sideBtn
-// window.addEventListener("scroll", function () {
-//   const sideBtna = this.document.querySelectorAll(".sideBtn a");
-//   const sideBtn = this.document.querySelector(".sideBtn");
-//   const triggerHeight = 5740;
-
-//   if (this.window.scrollY > 200) {
-//     sideBtna.style.opacity = "1";
-//   } else {
-//     sideBtna.style.opacity = "0";
-//   }
-//   if (this.window.pageYOffset > triggerHeight) {
-//     sideBtn.classList.add("on");
-//   } else {
-//     sideBtn.classList.remove("on");
-//   }
-// });
